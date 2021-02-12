ef43caf99433635d74894d8a4b13d260d8de6b3e
Evan Hsu <evanlhsu@gmail.com>
2021-02-12 16:03:24 -0500

    Add form element to ToDoList component - Input element & button element

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -2,12 +2,23 @@ import {Component} from 'react'
 import {get, post} from "../api"
 
 class TodoList extends Component {
+    // get("/test")
+    // .then(resp => {
+
+    // }).catch(err => {
+
+    // })
+
     render() {
         return (
             <div>
                 <h1>Todo List</h1>
+                <form name="TodoForm">
+                    <input type="text" name="todo" id="name" placeholder="Enter task" />
+                    <button type="submit">Add</button>
+                </form>
             </div>
-        ) 
+        )
     }
 }
 
