15805bf9425d936674d2027b6bd0ca60441fff22
Evan Hsu <evanlhsu@gmail.com>
2021-02-13 11:40:02 -0500

    Implement flask routing for GET and POST requests

--- server/app.py
+++ server/app.py
@@ -5,11 +5,16 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
 
-
-@app.route("/index", methods=["GET"])
-def index(): 
-    return jsonify(message="Todo app is running")
-
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return jsonify(message="Get request")
+    elif request.method == "POST":
+        post = {"task": "hello"}
+        mongo.db.todos.insert_one(post)
+        return jsonify(message="Post request")
+    else:
+        return jsonify(message="Request Not Supported")
 
 if __name__ == "__main__":
     app.run(port=4000, debug=True)
