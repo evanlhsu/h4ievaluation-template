5e19b421bc9f1247cca9d6a6d1da3ba0a78dda52
Evan Hsu <evanlhsu@gmail.com>
2021-02-17 14:38:34 -0500

    Commit rest of project

--- client/src/components/ItemsList.js
+++ client/src/components/ItemsList.js
@@ -2,16 +2,11 @@ import {Component} from 'react'
 import TodoItem from './TodoItem'
 
 class ItemsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    }
-  }
-
   render() {
     return (
-      this.props.items.map((item, index) => <TodoItem item={item} index={index} />)
+      <ul>
+        {this.props.items.map((item, index) => <TodoItem item={item} index={index} deleteItem={this.props.deleteItem}/>)}
+      </ul>
     )
   }
 }
