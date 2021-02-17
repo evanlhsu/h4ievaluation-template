1110eeecd1594abab997a41b371ce15f55a06602
Evan Hsu <evanlhsu@gmail.com>
2021-02-17 14:37:34 -0500

    Add onclick event handler for deleting item

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -1,12 +1,23 @@
 import {Component} from 'react'
+import axios from 'axios'
 
 class TodoItem extends Component {
+  constructor(props) {
+    super(props)
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick = (event) => {
+    event.preventDefault();
+    this.props.deleteItem(this.props.index)
+  }
+
   render() {
     return (
       <li key={this.props.index}>
         {this.props.item}
-        <div class="divider"/>
-        <button type="submit">
+        <div className="divider"/>
+        <button onClick={this.onClick} type="submit" value="submit">
           Delete
         </button>
       </li>
